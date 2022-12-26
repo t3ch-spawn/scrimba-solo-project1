8b2cd17016f589488b358f3576502af46a8041ad
Tife <boluwatife1904@gmail.com>
2022-12-26 13:30:37 +0100

    edited typography

--- src/components/interests-1.jsx
+++ src/components/interests-1.jsx
@@ -5,7 +5,7 @@ function Interests() {
       <p>
         When I'm not studying nor buried in code, I'm either spending time with
         friends, gaming or playing table-tennis. I like to go to the movies
-        occasionally other times I enjoy some solitude
+        occasionally, other times I enjoy some solitude
       </p>
     </div>
   );
