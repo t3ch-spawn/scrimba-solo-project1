4b8b8c96cb8e3b2306ee21b5b666a63184e927d5
Tife <boluwatife1904@gmail.com>
2022-12-26 11:23:08 +0100

    added new files, finished building (with mobile view)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Card_1 from "./components/card-1";
+import Card_2 from "./components/card-2";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Card_1 />
+      <Card_2 />
+    </div>
+  );
 }
 
 export default App;
