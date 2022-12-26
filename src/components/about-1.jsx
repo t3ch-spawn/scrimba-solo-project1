a1a1d6bafae6f8380ee6b6636bb88d66305e9097
Tife <boluwatife1904@gmail.com>
2022-12-26 19:20:45 +0100

    edited typography

--- src/components/about-1.jsx
+++ src/components/about-1.jsx
@@ -3,8 +3,8 @@ function About() {
     <div className="about">
       <h4>About</h4>
       <p>
-        I am Industrial Engineering undergraduate and a frontend developer with
-        a particular interest in making things simple and automating daily
+        I am an Industrial Engineering undergraduate and a frontend developer
+        with a particular interest in making things simple and automating daily
         tasks. I try to keep up with security and best practices, and I'm always
         looking for new things to learn. I love collaborating with other
         developers and designers to work on creative projects
