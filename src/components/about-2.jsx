50a9dd61b45447f419363fe6e10fbbb5107506f5
Tife <boluwatife1904@gmail.com>
2022-12-26 13:29:25 +0100

    edited typography

--- src/components/about-2.jsx
+++ src/components/about-2.jsx
@@ -6,7 +6,7 @@ function About_2() {
         I am Industrial Engineering undergraduate and a frontend developer with
         a particular interest in making things simple and automating daily
         tasks. I try to keep up with security and best practices, and I'm always
-        looking for new things to learn. I love collaborating with othe
+        looking for new things to learn. I love collaborating with other
         developers and designers to work on creative projects
       </p>
     </div>
--- src/components/interests-2.jsx
+++ src/components/interests-2.jsx
@@ -3,9 +3,9 @@ function Interests_2() {
     <div className="interests interests--2">
       <h4>Interests</h4>
       <p>
-        When I'm not buried in code, I'm either spending time with friends,
-        gaming or playing table-tennis. I like to go to the movies occasionally.
-        And sometimes I enjoy some solitude
+        When I'm not studying nor buried in code, I'm either spending time with
+        friends, gaming or playing table-tennis. I like to go to the movies
+        occasionally other times I enjoy some solitude
       </p>
     </div>
   );
